0db9a878c93249c5e9384f3bba02fa12778905f8
kekedezha <dezhac6@gmail.com>
2025-01-30 15:39:40 -0500

    type module, basic set up for api

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { Pool } from "pg";
+
+const app = express();
+const port = 3000;
+
+const pool = new Pool({
+  user: "me",
+  host: "localhost",
+  database: "api",
+  password: "password",
+  port: 5432,
+});
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// GET route for root '/' URL
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+// Have app start listening on specified port
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
