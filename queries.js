889b9f501037c259eccd2147dd702d958ba9c9c1
kekedezha <dezhac6@gmail.com>
2025-01-31 16:25:39 -0500

    updated queries file with POST endpoints

--- queries.js
+++ queries.js
@@ -20,6 +20,39 @@ export const getUserById = (request, response) => {
     if (error) {
       throw error;
     }
-    response.status(200).json(results.row);
+    response.status(200).json(results.rows);
   });
+};
+
+// POST endpoint to update table with a get user
+export const createUser = (request, response) => {
+  const { name, email } = request.body;
+
+  pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2)",
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
+
+// POST endpoint to update data for an existing user
+export const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  const { name, email } = request.body;
+
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
 };
