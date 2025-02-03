f64b55796c6043ffef1b1b8bb11367c39399e64b
kekedezha <dezhac6@gmail.com>
2025-02-03 15:49:47 -0500

    updated POST function to return new db entry to be able to return id in the reponse for POST route in API

--- queries.js
+++ queries.js
@@ -36,13 +36,13 @@ export const createUser = (request, response) => {
   const { name, email } = request.body;
 
   pool.query(
-    "INSERT INTO users (name, email) VALUES ($1, $2)",
+    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *",
     [name, email],
     (error, results) => {
       if (error) {
         throw error;
       }
-      response.status(201).send(`User added with ID: ${results.insertId}`);
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
     }
   );
 };
